350e80b7dbc75902a03bc029d5ff2d25518bb84c
santatra <razafindratsimba34@gmail.com>
2024-03-31 20:57:20 +0300

    add context configuration

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -5,6 +5,13 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+	var connectionString = "server=localhost;user=root;password=root;database=shopping_cart";
+	var serverVersion = new MariaDbServerVersion(new Version(15, 1));
+	optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+
     public DbSet<ShoppingCartObj> ShoppingCartObjects { get; set; }
 
 }
