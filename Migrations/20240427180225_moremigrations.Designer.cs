c2ff61ff7012c2f7b9c11aafb20d706eead71796
santatra <razafindratsimba34@gmail.com>
2024-04-27 21:05:37 +0300

    migration: more migrations

--- Migrations/20240427180225_moremigrations.Designer.cs
+++ Migrations/20240427180225_moremigrations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingCart.Models;
 
@@ -10,9 +11,11 @@ using ShoppingCart.Models;
 namespace ShoppingCart.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240427180225_moremigrations")]
+    partial class moremigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
